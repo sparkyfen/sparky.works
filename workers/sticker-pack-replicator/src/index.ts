18513258c605a1ae71a119a63f433cc30dc8fb01
Sparky <sparky1010@gmail.com>
2022-08-28 11:45:42 -0700

    [WIP] Worktop testing.

--- workers/sticker-pack-replicator/src/index.ts
+++ workers/sticker-pack-replicator/src/index.ts
@@ -1,61 +1,49 @@
-import { Markup, Scenes, Telegraf, session } from 'telegraf';
-import { Application, Router } from '@cfworker/web';
-import createTelegrafMiddware from 'cfworker-middware-telegraf';
+import { Bot, webhookCallback } from 'grammy';
+import { Router as WorktopRouter } from 'worktop';
+import { listen } from 'worktop/cache';
+import * as CORS from 'worktop/cors';
 
-import { DEFAULT_CORS_HEADERS } from './utils';
+const bot = new Bot(TELEGRAM_API_TOKEN);
+const API = new WorktopRouter();
 
-const router = new Router();
-
-const bot = new Telegraf(TELEGRAM_API_TOKEN);
-
-const transferScene = new Scenes.BaseScene('transfer');
-transferScene.enter((ctx) => ctx.reply('Please send the link of the sticker pack or a sticker from the pack.'));
-transferScene.leave((ctx) => ctx.reply('The command was cancelled.\nSend /help for a list of commands.'));
-transferScene.command('cancel', Scenes.Stage.leave())
-transferScene.on('text', (ctx) => {
-  return ctx.reply(`Received message: ${ctx.message.text}`);
-})
-transferScene.on('sticker', (ctx) => ctx.reply('Received sticker!'));
-transferScene.on('message', (ctx) => ctx.reply('Sorry this input is unaccepted.'));
-
-const stage = new Scenes.Stage([transferScene]);
-bot.use(stage.middleware());
-
-bot.start((ctx) => ctx.reply(`Hello there!\n\nThis bot has the ability to copy stickers from one pack to another.\nNote: It won't retain stats on the previous one or update/delete the old pack.\nTo get started, type /transfer with a link or follow it up with a sticker.`));
-bot.help((ctx) => ctx.reply('TODO!'));
-bot.command('ping', (ctx) => {
-  return ctx.reply('pong');
+bot.command('start', async (ctx) => 
+  ctx.reply(`Hello there!\n\nThis bot has the ability to copy stickers from one pack to another.\nNote: It won't retain stats on the previous one or update/delete the old pack.\nTo get started, type /transfer with a link or follow it up with a sticker.`, {
+  reply_to_message_id: ctx.message.msg.message_id,
+}));
+bot.command('help', async (ctx) => ctx.reply('TODO!', {
+  reply_to_message_id: ctx.message.msg.message_id,
+}));
+bot.command('ping', async (ctx) => {
+  return ctx.reply('pong', {
+    reply_to_message_id: ctx.message.msg.message_id,
+  });
 });
-bot.command('transfer', (ctx) => {
-  if(ctx.message.from.id != SPARKY_TELEGRAM_ID) {
-    return ctx.reply('Work in progress, come back soon <3.');
-  }
-  return Scenes.Stage.enter('transfer');
+bot.command('cancel', async (ctx) => {
+  return ctx.reply('There was no active command to cancel', {
+    reply_to_message_id: ctx.message.msg.message_id,
+  });
 });
+bot.command('stop', async (ctx) => ctx.leaveChat());
+bot.command('quit', async (ctx) => ctx.leaveChat());
+bot.on('message:text', async (ctx) => ctx.reply('Got another message!', {
+  reply_to_message_id: ctx.message.msg.message_id,
+}));
+bot.catch(err => console.log(err));
 
-bot.command('echo', (ctx) => {
-  return ctx.reply('echo');
-});
-
-bot.command('cancel', (ctx) => {
-  return ctx.reply('There was no active command to cancel');
-});
-bot.command('stop', (ctx) => ctx.leaveChat());
-bot.command('quit', (ctx) => ctx.leaveChat());
-bot.on('message', (ctx) => ctx.reply('Got another message!'));
-
-// Simple CORS middleware.
-const cors = async ({res}, next) => {
-  for (const HEADER_KEY in DEFAULT_CORS_HEADERS) {
-    res.headers.set(HEADER_KEY, DEFAULT_CORS_HEADERS[HEADER_KEY]);
-  }
-  await next();
-};
-
-router.get('/', ({res}) => {
-  res.headers.set('Content-Type', 'application/json;charset=UTF-8');
-  res.body = JSON.stringify({message: 'Hello world!'});
+/**
+ * Handles `OPTIONS` requests using the same settings.
+ * NOTE: Call `CORS.preflight` per-route for individual settings.
+ */
+API.prepare = CORS.preflight({
+  origin: '*', // allow any `Origin` to connect
+  headers: ['Cache-Control', 'Content-Type'],
+  methods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE'],
 });
 
-router.post(`/${TELEGRAM_SECRET_PATH}`, createTelegrafMiddware(bot));
-new Application().use(cors).use(router.middleware).listen();
+API.add('GET', '/', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+  res.send(200, JSON.stringify({message: 'Hello world!'}))
+});
+
+API.add('POST', `/${TELEGRAM_SECRET_PATH}`, (req, res) => webhookCallback(bot, 'worktop')(req, res));
+listen(API.run);
--- workers/sticker-pack-replicator/webpack.config.js
+++ workers/sticker-pack-replicator/webpack.config.js
@@ -12,8 +12,28 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".js"],
     fallback: {
+      path: require.resolve('path-browserify'),
+      stream: require.resolve('stream-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      url: require.resolve('url/'),
+      buffer: require.resolve('buffer/'),
       fs: false,
     },
+  },
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
   },
   plugins: [new NodePolyfillPlugin()],
   performance: {
